9968dfe00ce53b9d6919e2fc7b26d1daae35ae6d
AndyDev77 <andyvespuce77550@gmail.com>
2024-03-25 13:11:19 +0100

    update database

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,17 @@
+import Title from "@/components/Title";
+import TransformationForm from "@/components/TransformationForm";
+
 const Dashboard = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <Title
+        title="Ajouter une vidéo"
+        subtitle="Création d'une card avec une image et une vidéo"
+      />
+
+      <TransformationForm />
+    </>
+  );
 };
 
 export default Dashboard;
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks/clerk"],
+  publicRoutes: ["/sign-in", "/api/webhooks/clerk"],
   ignoredRoutes: ["/"],
 });
 
